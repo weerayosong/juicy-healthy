96a4af44a3f2f45f8d2bb8c558d906ea7566ac90
weerayosong <weerayosong@outlook.com>
2026-05-09 22:59:23 +0700

    change  node to 20

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error) {
         console.error(`Error: ${error.message}`)
-        process.exit(1) // จำให้แม่น!!! สั่งปิดโปรเซสถ้าเชื่อมต่อไม่ได้
+        // process.exit(1)
     }
 }
 
